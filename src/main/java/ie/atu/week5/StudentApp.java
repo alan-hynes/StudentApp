17dc93ffc33dcc3dd39d54231f661aaf120eac1c
G00400498 <G00400498@atu.ie>
2022-10-18 14:09:06 +0100

    New project for Student App

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -1,4 +1,16 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class StudentApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the student name");
+
+        System.out.println("Please enter the student email");
+
+        System.out.println("Please enter the student course");
+
+    }
 }
